fcd8540017deed051fb6e25caec546da6e1ad3fc
Ratikant Sahoo <147396516+Ratikant-05@users.noreply.github.com>
2024-12-08 11:17:50 +0000

    variables & datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 15440
+let accountEmail = 'ratikant@gmail.com'
+var accountPassword = 'Rati@12345'
+accountCity = "Delhi"
+let accountState;
+
+// accountId = 50
+
+// console.log(accountId); 
+// not allowed
+
+accountEmail = 'Gudday@123'
+accountPassword = '54321'
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+// Primitive Datatype
+
+let name = "Rati"
+let number = 9310088262
+let boolean = true //false
+
+// BigInt()
+// Symbol
+// null
+let a = null
+let b;
+
+console.table([name,number,boolean,a,b])
+
+// Non Primitive Datatype
+
+// Object
+
+console.table([typeof(name),typeof(number),typeof(boolean),typeof(a),typeof(b)])
+
+
+// the type of null is an object
+// the type of undefined is undefined
